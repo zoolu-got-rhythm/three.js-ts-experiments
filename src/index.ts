b8bac13166094490e6959a67b4018434a32a8a58
christopher morris <christopher.pkm@gmail.com>
2021-12-20 03:30:44 +0000

    need to add some random direction movement now to spheres

--- src/index.ts
+++ src/index.ts
@@ -1,103 +1,74 @@
 import * as THREE from 'three';
-import { Plane } from 'three';
+import { Color, Plane } from 'three';
 import { BufferGeneric } from './BufferGeneric';
+import { gsap } from 'gsap';
 
-class MatrixChar{
-	updateSpeed: number = 100; // update speed in ms
-	timeElapsedSinceLastUpdate: number = 0;
-	char: string;
-	plane: THREE.Mesh<THREE.PlaneGeometry, THREE.MeshBasicMaterial>;
 
-	constructor(char, position: THREE.Vector3){
-		this.char = char;
-		const geometry = new THREE.PlaneGeometry( 1, 1 );
-		const material = new THREE.MeshBasicMaterial( 
-			{color: "lime", side: THREE.DoubleSide, transparent: true} 
-		);
-		this.plane = new THREE.Mesh( geometry, material );
-		this.plane.position.copy(position);
-	}
-
-	getMesh(){
-		return this.plane;
-	}
-
-	update(time: number){
-		if(!(time - this.timeElapsedSinceLastUpdate >= this.updateSpeed))
-			return;
-		this.plane.material.opacity -= 0.05;
-		this.updateTimeElapsedSinceLastUpdate(time);
-	}
-
-	updateTimeElapsedSinceLastUpdate(time: number){
-		this.timeElapsedSinceLastUpdate = time;
-	}
-}
-
-class MatrixCharRain{
-	startingPos: THREE.Vector3;
-	bufferMatrixChar: BufferGeneric<MatrixChar>;
-	spawnNewMatrixCharSpeed: number = 200; // update speed in ms
-	timeElapsedSinceLastUpdate: number = 0;
-	sceneRef: THREE.Scene; 
-	constructor(scene: THREE.Scene, startingPos: THREE.Vector3, size: number){
-		this.startingPos = startingPos;
-		this.sceneRef = scene;
-		this.bufferMatrixChar = new BufferGeneric(size);
-		const matrixChar = new MatrixChar("", new THREE.Vector3(startingPos.x, startingPos.y, startingPos.z));
-		this.sceneRef.add(matrixChar.getMesh());
-		this.bufferMatrixChar.add(matrixChar);
-	}
-
-	add(){
-		let vectorOfPrevMatrixCharInBuffer = this.bufferMatrixChar.buffer[this.bufferMatrixChar.buffer.length - 1].getMesh().position;
-		const matrixChar = new MatrixChar("", new THREE.Vector3(
-			vectorOfPrevMatrixCharInBuffer.x, vectorOfPrevMatrixCharInBuffer.y-1, vectorOfPrevMatrixCharInBuffer.z
-		));
-		this.sceneRef.add(matrixChar.getMesh());
-		const matrixCharToRemove = this.bufferMatrixChar.add(matrixChar);
-		if(matrixCharToRemove)
-			this.sceneRef.remove(matrixCharToRemove.getMesh());
-	}
-
-	update(time: number){
-		if((time - this.timeElapsedSinceLastUpdate >= this.spawnNewMatrixCharSpeed)){
-			console.log("add");
-			this.add();
-			this.updateTimeElapsedSinceLastUpdate(time);
-		}
-		this.bufferMatrixChar.buffer.forEach((matrixChar: MatrixChar) => matrixChar.update(time));
-	}
-
-	updateTimeElapsedSinceLastUpdate(time: number){
-		this.timeElapsedSinceLastUpdate = time;
-	}
-}
-
-init();
 
 let camera, scene, renderer, codeRain;
 let geometry, material, mesh;
+const clock = new THREE.Clock(true);
+let time;
+
+const meshes: any[] = [];
+const n = 5; 
 
 function init() {
 
 	camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 50 );
-	camera.position.z = 20;
+	camera.position.z = 3;
 
 	scene = new THREE.Scene();
 
-	codeRain = new MatrixCharRain(scene, new THREE.Vector3(0,5,0), 10);
+	for(let i = 0; i < 5; i++){
+		const geometry = new THREE.SphereGeometry(0.5,10,10);
+		const material = new THREE.MeshNormalMaterial();
+		material.transparent = true;
+		const mesh = new THREE.Mesh(geometry, material);
+		meshes.push(mesh);
+	}
+	
+	meshes.forEach((mesh) => scene.add(mesh));
+
 	renderer = new THREE.WebGLRenderer( { antialias: true } );
 	renderer.setSize( window.innerWidth, window.innerHeight );
-	renderer.setAnimationLoop( animation );
 	document.body.appendChild( renderer.domElement );
+	time = clock.getElapsedTime();
+	// gsap.to(mesh.position, {duration: 0.2, x: 1, delay: 2});
+	// gsap.time
+	// gsap.is
+
+	tick();
 }
 
-function animation( time ) {	
-	console.log(time);
+
+
+function tick( ) {	
+	const currentTime = clock.getElapsedTime();
+	const delta = currentTime - time;
+	time = currentTime;
+
+	// camera.position.x = Math.sin(time);
+	// camera.position.y = Math.cos(time);
+	meshes.forEach((mesh, i) => {
+		const pos = ((Math.PI * 2) / n) * i;
+		mesh.position.x = Math.sin(pos + time * 0.5) * 2;	
+		mesh.position.z = Math.cos(pos + time * 0.5) * 2;
+		// mesh.material.opacity = Math.cos(pos + time * 0.5);
+		// console.log(mesh);
+	});
+	
+
+	// camera.lookAt(mesh.position);
+
+	// camera.fo
+
 	// console.log(delta);
-	// console.log(codeRain);
-	codeRain.update(time);
+
 	renderer.render( scene, camera );
+	window.requestAnimationFrame(tick);
 }
+
+init();
+
 
